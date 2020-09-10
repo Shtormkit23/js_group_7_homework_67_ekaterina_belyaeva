ccdca405f2327f957a60e059d3d88286e729d9ad
Belyaeva Ekaterina <katrinamalinashtorm@gmail.com>
2020-09-10 14:52:00 +0600

    Добавлен reducer.js

---
+++ src/store/reducer.js
@@ -0,0 +1,41 @@
+const initialState = {
+    code: '',
+    resultColor: '',
+    string: ''
+};
+
+const correctCode = '1234';
+
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case 'ADD':
+            const newState = { ...state };
+            if (!state.string.includes('*') && state.string !== '') {
+                newState.string = '*';
+                newState.resultColor = '';
+                newState.code = action.code;
+                return newState;
+            }
+            if (state.code.length === 4) {
+                return newState;
+            }
+
+            return { ...state, code: state.code + action.code, string: state.string + '*' };
+
+        case 'REMOVE':
+            return { ...state, string: state.string.slice(0, -1), code: state.code.slice(0, -1) };
+
+        case 'ENTER':
+            if (state.code === correctCode) {
+                return { ...state, resultColor: 'green', string: 'Access granted' };
+            } else {
+                return { ...state, resultColor: 'red', string: 'Access Denied' }
+            }
+        default:
+            return state;
+    }
+};
+
+
+export default reducer;
